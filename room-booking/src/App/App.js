1d15aa031b1c6b00e765cfd49189f17aef43d692
Navani5821 <navanithan9710@gmail.com>
2024-11-19 21:47:33 +0530

    first commit

---
+++ room-booking/src/App/App.js
@@ -0,0 +1,38 @@
+import "./App.css";
+
+// import react-router-dom
+import { Route, Routes } from "react-router-dom";
+
+// imports pages
+import Home from "../Pages/Home";
+import Room from "../Pages/Room";
+import SingleRoom from "../Pages/SingleRoom";
+import Error from "../Pages/Error";
+
+// import components
+import Navbar from "../Components/Navbar/Navbar";
+import Footer from "../Components/Footer/Footer";
+
+function App() {
+  return (
+    <>
+      <Navbar />
+      <Routes>
+        {/* Define the route for the Home page */}
+        <Route path="/" element={<Home />} />
+
+        {/* Define the route for the Room page */}
+        <Route path="/rooms" element={<Room />} />
+
+        {/* Define the route for individual rooms */}
+        <Route path="/rooms/:slug" element={<SingleRoom />} />
+
+        {/* Catch-all route for non-existent pages */}
+        <Route path="*" element={<Error />} />
+      </Routes>
+      <Footer />
+    </>
+  );
+}
+
+export default App;
