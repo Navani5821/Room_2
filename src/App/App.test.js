5a613dc44780af2494dbcd9580f2d16abcfa649d
Navani5821 <navanithan9710@gmail.com>
2024-11-22 16:17:28 +0530

    first commit

--- src/App/App.test.js
+++ src/App/App.test.js
@@ -1,7 +1,7 @@
 import { act } from "react";
 import { render, screen } from "@testing-library/react";
 import { BrowserRouter as Router } from "react-router-dom";
-import App from "./App";
+import App from "../App/App";
 import { RoomProvider } from "../Context/Context";
 
 test("renders luxurious rooms banner", () => {
--- src/index.js
+++ src/index.js
@@ -1,16 +1,28 @@
-import { render, screen } from "@testing-library/react";
-import { BrowserRouter as Router } from "react-router-dom";
-import { RoomProvider } from "../Context/Context"; // Update path if needed
-import App from "../App/App";
+import React from "react";
+import ReactDOM from "react-dom/client"; // Importing from "react-dom/client"
+import reportWebVitals from "./reportWebVitals";
+import App from "./App/App";
+import "./index.css";
 
-test("renders learn react link", () => {
-  render(
-    <RoomProvider>
-      <Router>
-        <App />
-      </Router>
-    </RoomProvider>
-  );
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
+// import react-router-dom
+import { BrowserRouter as Router } from "react-router-dom";
+
+// import context
+import { RoomProvider } from "./Context/Context";
+
+// Get the root element
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+// Render the app
+root.render(
+  <RoomProvider>
+    <Router>
+      <App />
+    </Router>
+  </RoomProvider>
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
