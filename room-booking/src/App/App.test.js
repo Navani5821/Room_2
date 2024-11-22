cf22037561d428f492548d14f294098cabfc6536
Navani5821 <navanithan9710@gmail.com>
2024-11-22 13:12:33 +0530

    first commit

--- room-booking/src/App/App.test.js
+++ room-booking/src/App/App.test.js
@@ -1,8 +1,21 @@
+import { act } from "react";
 import { render, screen } from "@testing-library/react";
+import { BrowserRouter as Router } from "react-router-dom";
 import App from "./App";
+import { RoomProvider } from "../Context/Context";
 
-test("renders learn react link", () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+test("renders luxurious rooms banner", () => {
+  act(() => {
+    render(
+      <RoomProvider>
+        <Router>
+          <App />
+        </Router>
+      </RoomProvider>
+    );
+  });
+
+  const bannerText = screen.getByText(/luxurious rooms/i);
+  expect(bannerText).toBeInTheDocument();
 });
+
--- room-booking/src/index.js
+++ room-booking/src/index.js
@@ -1,25 +1,16 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import reportWebVitals from "./reportWebVitals";
-import App from "./App/App";
-import "./index.css";
-
-// import react-router-dom
+import { render, screen } from "@testing-library/react";
 import { BrowserRouter as Router } from "react-router-dom";
+import { RoomProvider } from "../Context/Context"; // Update path if needed
+import App from "../App/App";
 
-// import context
-import { RoomProvider } from "./Context/Context";
-
-ReactDOM.render(
-  <RoomProvider>
-    <Router>
-      <App />
-    </Router>
-  </RoomProvider>,
-  document.getElementById("root")
-);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+test("renders learn react link", () => {
+  render(
+    <RoomProvider>
+      <Router>
+        <App />
+      </Router>
+    </RoomProvider>
+  );
+  const linkElement = screen.getByText(/learn react/i);
+  expect(linkElement).toBeInTheDocument();
+});
